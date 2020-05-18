3ed254afaa86bd0f78b8e89e01326dae1ba203a6
evmcodi <54327221+evmcodi@users.noreply.github.com>
2020-05-18 22:50:53 +0100

    feat: Add search function for users to search the food db on mongodb.

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
+mydatabase = mongo.db.OpenGenericFoodFacts
+
+# Create a text index so items in the index can be searched.
+mydatabase.create_index([('name_en', 'text')])
+
 
 @app.route('/')
 @app.route('/today')
@@ -18,11 +23,27 @@ def today():
 
 @app.route('/get_foods')
 def get_foods():
-    return render_template("foods.html", foods=mongo.db.OpenGenericFoodFacts.find())
+    mybeetroot = "beetroot"
+    return render_template("foods.html",
+                           # foods=mongo.db.OpenGenericFoodFacts.find())
+                           # foods = mydatabase.find())
+                           foods=mydatabase.find({"$text": {"$search": mybeetroot}}))
+
+
+@app.route('/search_foods')
+def search_foods():
+    search = request.args.get('search')
+    #   return redirect(url_for('success', name=user))
+    return render_template("foods.html",
+                           foods=mydatabase.find({"$text": {"$search": search}}))
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-
