a8c852edf001688085ec94923875a6d10bae3823
evmcodi <54327221+evmcodi@users.noreply.github.com>
2020-05-22 06:08:03 +0100

    feat: Add function that calculates nutrient values corresponding to user input of food weight and stores the calculations in global variables.

--- static/js/script.js
+++ static/js/script.js
@@ -21,23 +21,44 @@ $('#myModal').on('shown.bs.modal', function () {
 
 
 
+// Declare global variables
+var name_en;
+var energy_100g_cal;
+var fat_100g;
+var saturates_100g;
+var carb_100g;
+var sugars_100g;
+var fibre_100g;
+var protein_100g;
+var salt_100g;
+
+var calc_energy_100g_cal;
+var calc_fat_100g;
+var calc_saturates_100g;
+var calc_carb_100g;
+var calc_sugars_100g;
+var calc_fibre_100g;
+var calc_protein_100g;
+var calc_salt_100g;
+
+
 
 
 // Handle food data loading in modal for 'add food to today'
 $('#addToTodayModal').on('show.bs.modal', function (event) {
     console.log('success')
     var button = $(event.relatedTarget) // Button that triggered the modal
-    
-    // Extract info from data-* attributes
-    var name_en = button.data('name_en') 
-    var energy_100g_cal = button.data('energy_100g_cal') 
-    var fat_100g = button.data('fat_100g') 
-    var saturates_100g = button.data('saturates_100g') 
-    var carb_100g = button.data('carb_100g') 
-    var sugars_100g = button.data('sugars_100g') 
-    var fibre_100g = button.data('fibre_100g') 
-    var protein_100g = button.data('protein_100g')
-    var salt_100g = button.data('salt_100g')
+
+    // Extract info from data-* attributes and assign them to global variables.
+    name_en = button.data('name_en')
+    energy_100g_cal = button.data('energy_100g_cal')
+    fat_100g = button.data('fat_100g')
+    saturates_100g = button.data('saturates_100g')
+    carb_100g = button.data('carb_100g')
+    sugars_100g = button.data('sugars_100g')
+    fibre_100g = button.data('fibre_100g')
+    protein_100g = button.data('protein_100g')
+    salt_100g = button.data('salt_100g')
 
     console.log(protein_100g)
     console.log(typeof protein_100g)
@@ -50,14 +71,27 @@ $('#addToTodayModal').on('show.bs.modal', function (event) {
 
 
 
-    // var food = JSON.parse(clicked_food);
-
-    // var name_en = food.name_en
-
-    // console.log(name_en)
- 
-
     // var deletefoodurl = '/delete_food/' + clicked_food_id
     // modal.find('.deletefromdb_form').attr('action', deletefoodurl);
 
 })
+
+
+
+function calculateNutrients(val) {
+    // Calculate the nutrition values based on the user input
+    // var gramsinput = document.getElementById('grams-input');
+
+    // calc_name_en = name_en
+    calc_energy_100g_cal = (energy_100g_cal / 100) * val;
+    calc_fat_100g = (fat_100g / 100) * val;
+    calc_saturates_100g = (saturates_100g / 100) * val;
+    calc_carb_100g = (carb_100g / 100) * val;
+    calc_sugars_100g = (sugars_100g / 100) * val;
+    calc_fibre_100g = (fibre_100g / 100) * val;
+    calc_protein_100g = (protein_100g / 100) * val;
+    calc_salt_100g = (salt_100g / 100) * val;
+
+    console.log(calc_energy_100g_cal);
+
+}
